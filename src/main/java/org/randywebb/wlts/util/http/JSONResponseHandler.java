cbedcd2dba515a140e585c84977c4ae894d8069a
Marc Page <marcallenpage@gmail.com>
2018-05-06 08:15:38 -0500

    updating documentation for JSON http response handler

--- src/main/java/org/randywebb/wlts/util/http/JSONResponseHandler.java
+++ src/main/java/org/randywebb/wlts/util/http/JSONResponseHandler.java
@@ -9,19 +9,29 @@ import java.io.InputStreamReader;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
+
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
+/** Handle a JSON response.
+	@param <JSONType> This is the type of JSON entity expected to be returned, either JSONObject or JSONArray.
  * @author randyw
  *
  */
 public class JSONResponseHandler<JSONType> implements ResponseHandler<JSONType> {
 
+	/// Can be used for logging debugging messages
 	private static Logger log = LoggerFactory.getLogger(JSONResponseHandler.class);
 
+	/** Converts an HTTP response into a JSON entity.
+		@param response The HTTP repsonse
+		@return The JSON entity that was returned. If there is a problem parsing the JSON, this may be null.
+		@throws ClientProtocolException If it wasn't an HTTP connection, or we didn't get 200 OK response from the server.
+		@throws IOException on network error
+	*/
 	@Override
 	public JSONType handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
 
@@ -32,13 +42,13 @@ public class JSONResponseHandler<JSONType> implements ResponseHandler<JSONType> 
 			log.trace("Response Status: " + response.getStatusLine().toString());
 		}
 
-		if(response.getStatusLine().getStatusCode() != 200)
-		{
+		if(response.getStatusLine().getStatusCode() != 200) {
 			throw new ClientProtocolException("Unable to retrieve response. Server returned: " + response.getStatusLine().toString());
 		}
 
 		try {
 			Object obj = parser.parse(new InputStreamReader(response.getEntity().getContent()));
+
 			jsonObj = (JSONType) obj;
 
 		} catch (UnsupportedOperationException | ParseException e) {
@@ -47,7 +57,5 @@ public class JSONResponseHandler<JSONType> implements ResponseHandler<JSONType> 
 
 		return jsonObj;
 	}
-
-
 
 }
