b65f38a6460c9627e1f696b81a3261bd8f957d8f
Marc Page <marcallenpage@gmail.com>
2018-05-06 19:44:28 -0500

    finishing up documentation

--- src/main/java/org/randywebb/wlts/util/KMLWriter.java
+++ src/main/java/org/randywebb/wlts/util/KMLWriter.java
@@ -13,6 +13,9 @@ import org.slf4j.LoggerFactory;
 */
 public class KMLWriter {
 
+	/** Can be used for logging debugging messages */
+	private static Logger log = LoggerFactory.getLogger(KMLWriter.class);
+
 	/** Any element that needs to be written to the file. */
 	public interface Item {
 		/** Writes the element to the output.
@@ -197,8 +200,6 @@ public class KMLWriter {
 
 		/** Should we tessellate (or follow the contour of the terrain)? */
 		private boolean tessellate;
-		/** What altitude mode should we use (or null for do not specify) */
-		private String altitude_mode;
 		/** List of coordinate strings in "long, lat, alt" format */
 		private ArrayList<String> coordinates;
 	}
@@ -250,210 +251,322 @@ public class KMLWriter {
 			output.append(prefix + "</Point>\n");
 		}
 
+		/** The latitude of the icon */
 		private double latitude;
+		/** The longitude of the icon */
 		private double longitude;
+		/** The altitude of the icon */
 		private double altitude;
+		/** Connect the point to the ground */
 		private boolean extrude;
-		private String altitude_mode;
 	}
 
+	/** The area to look at */
 	public static class LookAt implements Item {
 
-	public LookAt(double latitude, double longitude) {
-	this.latitude = latitude;
-	this.longitude = longitude;
-	this.heading = 0;
-	this.tilt = 0;
-	this.range = 100;
-	this.altitude = 0;
+		/**
+			Defaults to a range of 100. Heading defaults to 0.
+			@param latitude center latitude to look at
+			@param longitude center longitude to look at
+		*/
+		public LookAt(double latitude, double longitude) {
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.heading = 0;
+			this.tilt = 0;
+			this.range = 100;
+			this.altitude = 0;
+		}
+
+		/**
+			@param heading the heading to use.
+			@return this for call chaining
+		*/
+		public LookAt setHeading(double heading) {
+			this.heading = heading;
+			return this;
+		}
+
+		/**
+			@param range the range to use.
+			@return this for call chaining
+		*/
+		public LookAt setRange(double range) {
+			this.range = range;
+			return this;
+		}
+
+		/**
+			@param tilt the tilt to use.
+			@return this for call chaining
+		*/
+		public LookAt setTilt(double tilt) {
+			this.tilt = tilt;
+			return this;
+		}
+
+		/**
+			@param altitude the altitude to use.
+			@return this for call chaining
+		*/
+		public LookAt setAltitude(double altitude) {
+			this.altitude = altitude;
+			return this;
+		}
+
+		/** Writes out the LookAt.
+			@param prefix The indentation
+			@param output The stream to write to
+			@throws IOException on io error
+		*/
+		public void write(String prefix, FileWriter output) throws IOException {
+			output.append(prefix + "<LookAt>\n");
+			output.append(prefix + "\t<longitude>"+longitude+"</longitude>\n");
+			output.append(prefix + "\t<latitude>"+latitude+"</latitude>\n");
+			output.append(prefix + "\t<altitude>"+altitude+"</altitude>\n");
+			output.append(prefix + "\t<heading>"+heading+"</heading>\n");
+			output.append(prefix + "\t<tilt>"+tilt+"</tilt>\n");
+			output.append(prefix + "\t<range>"+range+"</range>\n");
+			output.append(prefix + "</LookAt>\n");
+		}
+
+		/** The center latitude */
+		private double latitude;
+		/** The center longitude */
+		private double longitude;
+		/** The center altitude */
+		private double altitude;
+		/** The heading */
+		private double heading;
+		/** The tilt */
+		private double tilt;
+		/** The range. Defaults to 100. */
+		private double range;
 	}
 
-	public LookAt setHeading(double heading) {
-	this.heading = heading;
-	return this;
+	/** Parent for tags that contain other items */
+	public static abstract class List extends ArrayList<Item> implements Item {
+
+		/**
+			@param tag The tag type
+			@param attributes The attributes to use, or null if there are no attributes. Attributes are of the form x="y".
+		*/
+		public List(String tag, String attributes) {
+			this.tag = tag;
+			this.attributes = attributes;
+		}
+
+		/** Writes out the tag and its contained items.
+			@param prefix The indentation
+			@param output The stream to write to
+			@throws IOException on io error
+		*/
+		public void write(String prefix, FileWriter output) throws IOException {
+			output.append(prefix + "<"+tag+(null == attributes ? "" : (" " + attributes))+">\n");
+
+			for (Item item : this) {
+				item.write(prefix + "\t", output);
+			}
+
+			output.append(prefix + "</"+tag+">\n");
+		}
+
+		private String tag;
+		private String attributes;
 	}
 
-	public LookAt setRange(double range) {
-	this.range = range;
-	return this;
-	}
-
-	public LookAt setTilt(double tilt) {
-	this.tilt = tilt;
-	return this;
-	}
-
-	public LookAt setAltitude(double altitude) {
-	this.altitude = altitude;
-	return this;
-	}
-
-	public void write(String prefix, FileWriter output) throws IOException {
-	output.append(prefix + "<LookAt>\n");
-	output.append(prefix + "\t<longitude>"+longitude+"</longitude>\n");
-	output.append(prefix + "\t<latitude>"+latitude+"</latitude>\n");
-	output.append(prefix + "\t<altitude>"+altitude+"</altitude>\n");
-	output.append(prefix + "\t<heading>"+heading+"</heading>\n");
-	output.append(prefix + "\t<tilt>"+tilt+"</tilt>\n");
-	output.append(prefix + "\t<range>"+range+"</range>\n");
-	output.append(prefix + "</LookAt>\n");
-	}
-
-	private double latitude;
-	private double longitude;
-	private double heading;
-	private double tilt;
-	private double range;
-	private double altitude;
-	}
-
-	public static class List extends ArrayList<Item> implements Item {
-
-	public List(String tag, String attributes) {
-	this.tag = tag;
-	this.attributes = attributes;
-	}
-
-	public void write(String prefix, FileWriter output) throws IOException {
-	output.append(prefix + "<"+tag+(null == attributes ? "" : (" " + attributes))+">\n");
-
-	for (Item item : this) {
-	item.write(prefix + "\t", output);
-	}
-
-	output.append(prefix + "</"+tag+">\n");
-	}
-
-	private String tag;
-	private String attributes;
-	}
-
+	/** Folder tag */
 	public static class Folder extends List {
 
-	public Folder() {
-	super("Folder", null);
+		/** Create a new folder tag */
+		public Folder() {
+			super("Folder", null);
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public Folder append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public Folder append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** Placemark tag (used for icons) */
 	public static class Placemark extends List {
 
-	public Placemark() {
-	super("Placemark", null);
+		/** Create Placemark tag */
+		public Placemark() {
+			super("Placemark", null);
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public Placemark append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public Placemark append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** IconStyle tag */
 	public static class StyleIcon implements Item {
 
-	public StyleIcon(String url) {
-	this.url = url;
+		/**
+			@param url The url of the icon
+		*/
+		public StyleIcon(String url) {
+			this.url = url;
+		}
+
+		/** Writes out the IconStyle.
+			@param prefix The indentation
+			@param output The stream to write to
+			@throws IOException on io error
+		*/
+		public void write(String prefix, FileWriter output) throws IOException {
+			output.append(prefix + "<IconStyle>\n");
+			output.append(prefix + "\t<Icon>\n");
+			output.append(prefix + "\t\t<href><![CDATA["+url+"]]></href>\n");
+			output.append(prefix + "\t</Icon>\n");
+			output.append(prefix + "</IconStyle>\n");
+		}
+
+		/** The url of the icon */
+		private String url;
+
 	}
 
-	public void write(String prefix, FileWriter output) throws IOException {
-	output.append(prefix + "<IconStyle>\n");
-	output.append(prefix + "\t<Icon>\n");
-	output.append(prefix + "\t\t<href><![CDATA["+url+"]]></href>\n");
-	output.append(prefix + "\t</Icon>\n");
-	output.append(prefix + "</IconStyle>\n");
-	}
-
-	private String url;
-
-	}
-
+	/** Width tag (for lines) */
 	public static class StyleWidth extends Simple {
-	public StyleWidth(double width) {
-	super("width", ""+width);
-	}
+
+		/**
+			@param width The width of the line for style.
+		*/
+		public StyleWidth(double width) {
+			super("width", ""+width);
+		}
+
 	}
 
+	/** Color tag (for lines) */
 	public static class StyleColor extends Simple {
-	public StyleColor(String color) {
-	super("color", color);
-	}
+
+		/**
+			@param color The color of the line for style.
+		*/
+		public StyleColor(String color) {
+			super("color", color);
+		}
+
 	}
 
+	/** LineStyle container */
 	public static class LineStyle extends List {
 
-	public LineStyle() {
-	super("LineStyle", null);
+		/** New LineStyle tag */
+		public LineStyle() {
+			super("LineStyle", null);
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public LineStyle append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public LineStyle append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** Style for polygons. */
 	public static class PolyStyle extends List {
 
-	public PolyStyle() {
-	super("PolyStyle", null);
+		/** New PolyStyle tag */
+		public PolyStyle() {
+			super("PolyStyle", null);
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public PolyStyle append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public PolyStyle append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** Style definition tag */
 	public static class Style extends List {
 
-	public Style(String name) {
-	super("Style", "id=\""+name+"\"");
+		/**
+			@param name The id name for the style
+		*/
+		public Style(String name) {
+			super("Style", "id=\""+name+"\"");
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public Style append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public Style append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** Open = true tag */
 	public static class Open extends Simple {
-	public Open() {
-	super("open", "1");
-	}
+
+		/** Creates a &lt;open&gt;1&lt;/open&gt; tag */
+		public Open() {
+			super("open", "1");
+		}
+
 	}
 
+	/** Document definition */
 	public static class Document extends List {
 
-	public Document() {
-	super("Document", null);
+		/** Create new document */
+		public Document() {
+			super("Document", null);
+		}
+
+		/**
+			@param item The sub-item to add to the list.
+			@return this for call chaining
+		*/
+		public Document append(Item item) {
+			add(item);
+			return this;
+		}
+
 	}
 
-	public Document append(Item item) {
-	add(item);
-	return this;
-	}
-
-	}
-
+	/** Write the document to a .kml file.
+		@param filename The path to the .kml file to create
+		@param document The document structure to write out
+		@throws IOException on io error
+	*/
 	public static void write(String filename, Document document) throws IOException {
-	FileWriter output = new FileWriter(filename);
+		FileWriter output = new FileWriter(filename);
 
-	output.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-	output.append("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n");
+		output.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		output.append("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n");
 
-	document.write("\t", output);
-	output.append("</kml>\n");
-	output.close();
+		document.write("\t", output);
+		output.append("</kml>\n");
+		output.close();
 	}
 
-	private static Logger log = LoggerFactory.getLogger(KMLWriter.class);
-	private FileWriter output;
-	private Folder currentFolder;
 }
